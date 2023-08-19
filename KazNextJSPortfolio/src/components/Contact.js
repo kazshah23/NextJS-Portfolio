7d17611d2e98a45a3597b1900effb469f395538d
Kazmain Shah <kshah228@illinois.edu>
2023-08-18 21:14:12 -0500

    Project

---
+++ KazNextJSPortfolio/src/components/Contact.js
@@ -0,0 +1,105 @@
+import emailjs from "emailjs-com";
+import { useState } from "react";
+const Contact = () => {
+  const [mailData, setMailData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const { name, email, message, subject } = mailData;
+  const [error, setError] = useState(null);
+  const onChange = (e) =>
+    setMailData({ ...mailData, [e.target.name]: e.target.value });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (
+      name.length === 0 ||
+      email.length === 0 ||
+      message.length === 0 ||
+      subject.length === 0
+    ) {
+      setError(true);
+      clearError();
+    } else {
+      emailjs
+        .send(
+          "service_seruhwu", // service id
+          "template_21aw58z", // template id
+          mailData,
+          "Q3pccdLZhU-mZT7tQ" // public api
+        )
+        .then(
+          (response) => {
+            setError(false);
+            clearError();
+            setMailData({ name: "", email: "", message: "", subject: "" });
+          },
+          (err) => {
+            console.log(err.text);
+          }
+        );
+    }
+  };
+  const clearError = () => {
+    setTimeout(() => {
+      setError(null);
+    }, 2000);
+  };
+  return (
+    <section
+      id="contactus"
+      data-nav-tooltip="Contact Me"
+      className="pp-section pp-scrollable section dark-bg"
+    >
+      <div className="container">
+        <div className="title">
+          <h3>Get in touch.</h3>
+        </div>
+        <div className="row">
+          <div className="col-lg-5 col-xl-4 m-15px-tb">
+            <div className="contact-info">
+              <h4>Get in touch</h4>
+              <p>
+                I answer every email fast!
+              </p>
+              <ul>
+                <li className="media">
+                  <i className="ti-map" />
+                  <span className="media-body">
+                   Hoffman Estates, Illinois
+                  </span>
+                </li>
+                <li className="media">
+                  <i className="ti-email" />
+                  <span className="media-body">kshah228@illinois.edu</span>
+                </li>
+                <li className="media">
+                  <i className="ti-email" />
+                  <span className="media-body">(847)-644-9333</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-7 col-xl-8 m-15px-tb">
+            {/* <div className="contact-form">
+              
+            </div> */}
+          </div>
+          <div className="col-12">
+            <div className="google-map">
+              <div className="embed-responsive embed-responsive-21by9">
+                <iframe
+                  className="embed-responsive-item"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47386.27950165534!2d-88.19275987572126!3d42.0722823455749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880f06183380f3c5%3A0xe48e4b938d23766e!2sHoffman%20Estates%2C%20IL!5e0!3m2!1sen!2sus!4v1692406140014!5m2!1sen!2sus" 
+                  allowFullScreen=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Contact;
